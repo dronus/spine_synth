c60b273565aac35914c42f04fbbf7924e8fc1407
dronus <paul.geisler@web.de>
2018-02-25 16:50:28 +0100

    added second envelope for fast amplitude and slow filter attack. beat tapping now counts four bars.

--- teenyTest2.ino
+++ teenyTest2.ino
@@ -3,22 +3,32 @@
 #include <SPI.h>
 #include <SD.h>
 #include <SerialFlash.h>
- 
+#include <math.h> 
+
 
 // GUItool: begin automatically generated code
 //AudioInputUSB            usb1;           //xy=138,124
+AudioSynthWaveformDc     dc1;
 AudioEffectEnvelope      env1;
+AudioEffectEnvelope      env2;
+AudioEffectMultiply      mult1;
 AudioSynthWaveform       osc1;
 AudioFilterStateVariable filter1;        //xy=340,125
 AudioFilterStateVariable filter2;        //xy=343,180
 AudioOutputAnalog        dac1;           //xy=546,73
 AudioOutputUSB           usb2;           //xy=548,127
-AudioConnection          patchCord0(osc1, 0, env1, 0);
-AudioConnection          patchCord1(env1, 0, filter1, 0);
-AudioConnection          patchCord2(env1, 0, filter2, 0);
-AudioConnection          patchCord3(filter1, 0, usb2, 0);
-AudioConnection          patchCord4(filter1, 0, dac1, 0);
-AudioConnection          patchCord5(filter2, 0, usb2, 1);
+//AudioConnection          patchCord0(osc1, 0, env1, 0);
+AudioConnection          patchCord3(dc1, 0, env1, 0);
+AudioConnection          patchCord31(dc1, 0, env2, 0);
+AudioConnection          patchCord0(osc1, 0, filter1, 0);
+AudioConnection          patchCord01(env1, 0, filter1, 1);
+AudioConnection          patchCord1(filter1, 0, filter2, 0);
+AudioConnection          patchCord11(env1, 0, filter2, 1);
+AudioConnection          patchCord2(filter2, 0, mult1, 0);
+AudioConnection          patchCord4(env2, 0, mult1, 1);
+AudioConnection          patchCord5(mult1, 0, dac1, 0);
+AudioConnection          patchCord6(mult1, 0, usb2, 0);
+AudioConnection          patchCord7(mult1, 0, usb2, 1);
 // GUItool: end automatically generated code
 
 #define FIVE_VOLT_TOLERANCE_WORKAROUND
@@ -33,14 +43,16 @@ void setup(void)
 {
   pinMode(22,INPUT_PULLDOWN);
   pinMode(23,INPUT_PULLDOWN);
+  pinMode(13,OUTPUT);
 
 //  AudioMemory(30);
   Serial.println("setup done");
   Serial.begin(9600);
-  while (!Serial) ;
+//  while (!Serial) ;
 
   AudioMemory(30);
-  osc1.begin(WAVEFORM_SQUARE);
+  dc1.amplitude(1.0);
+  osc1.begin(WAVEFORM_SAWTOOTH);
   osc1.pulseWidth(0.5);
   osc1.frequency(440.0);
   osc1.amplitude(1.0);
@@ -48,19 +60,39 @@ void setup(void)
 }
  
 long last_time;
+bool digitals_last[2];
+bool digitals_click[2];
 float analogs_slow[]={0.,0.,0.,0.,0.,0.};
 float last_volume=0.;
 int cycle=0;
+int cycle_length=150;
 int step =0;
 float frequencies[16];
 float volumes[16];
+
+long last_tap=0;
+
+long taps[4];
+
+float quantize(float f)
+{
+  return exp2(floor(log2(f)*12.0f)/12.0f);
+}
+
 void loop()
 {
 
-  int analogs[]={analogRead(A5),analogRead(A6),analogRead(A7),c1.capacitiveSensor(64),c2.capacitiveSensor(64)};
+  bool digitals[]={digitalRead(22),digitalRead(23)};
+  for(int i=0;i<2; i++)
+  {
+    digitals_click[i]=(!digitals_last[i] && digitals[i]);
+    digitals_last[i]=digitals[i];
+  }
+
+  int analogs[]={analogRead(A5),analogRead(A6),analogRead(A7),c1.capacitiveSensor(128),c2.capacitiveSensor(128)};
 
   for(int i=0; i<5; i++)
-    analogs_slow[i]=analogs_slow[i]*0.95+analogs[i]*0.05;
+    analogs_slow[i]=analogs_slow[i]*0.8+analogs[i]*0.2;
   
  /*
   for(int i=0; i<5; i++)
@@ -68,7 +100,7 @@ void loop()
     Serial.print(analogs[i]);
     Serial.print(" ");
   }
-  byte digitals[]={digitalRead(22),digitalRead(23)};
+
   for(int i=0; i<2; i++)
   {
     Serial.print(digitals[i]);
@@ -77,45 +109,67 @@ void loop()
   Serial.println();
 */
   float volume=analogs_slow[3]+analogs_slow[4];
-  float frequency=(analogs_slow[3]-analogs_slow[4])/volume+0.5f;
+  float frequency=(analogs_slow[3]-analogs_slow[4])/volume+1.0f;
   frequency*=100.0f;
   // Serial.print("f:");  Serial.print(frequency); Serial.println();
 
-  volume-=5000.0f;
+  volume-=14000.0f;
   if(volume<0.f) volume=0.f;
   volume/=5000.0f;
 
 
-  int cycle_length=analogs_slow[0];
+  // int cycle_length=analogs_slow[0];
 
   long time=millis();
   long dt=time-last_time;
   last_time = time;
+
+  // tap tempo
+  if(digitals_click[0])
+  {
+    long four_bars=time-taps[3];
+    cycle_length=four_bars/16;
+
+    for(int i=2; i>=0; i--)
+      taps[i+1]=taps[i];
+    taps[0]=time;
+  }
 
   cycle+=dt;
   if(cycle>cycle_length){
     step++;
     step=step % 16;
 
-    if(volume>0) frequencies[step]=frequency;
-    if(volume>0) volumes[step]    =volume;
+    if(volume>0){
+      frequencies[step]=quantize(frequency);
+      volumes[step]=volume;
+    }
+    if(digitals[1]) 
+      volumes[step]=0;
+
     osc1.frequency  (frequencies[step]);
-    osc1.amplitude  (volumes[step]);
 
-    if(volumes[step]>0) env1.noteOn();
-    else                env1.noteOff();
+    env1.attack(cycle_length*0.5);
+    env1.sustain(volumes[step]);
+    env2.sustain(volumes[step]);
+//osc1.amplitude(volumes[step]);
+    
+    if(volumes[step]>0) { env1.noteOn();  env2.noteOn(); }
+    else                { env1.noteOff(); env2.noteOff();}
 
-    digitalWrite(13,step%2==0);
+    digitalWrite(13,step%4==0);
 
     cycle-=cycle_length;
   }
 
 
   AudioNoInterrupts();
-  filter1.resonance(analogs_slow[1]*5.0f/1024.0f);
-  filter2.resonance(analogs_slow[1]*5.0f/1024.0f);
-  filter1.frequency((analogs_slow[2])*3.0f);
-  filter2.frequency((analogs_slow[2])*3.0f);
+  filter1.octaveControl(analogs_slow[1]*5.0f/1024.0f);
+  filter2.octaveControl(analogs_slow[1]*5.0f/1024.0f);
+  filter1.resonance(analogs_slow[2]*5.0f/1024.0f);
+  filter2.resonance(analogs_slow[2]*5.0f/1024.0f);
+  filter1.frequency((1024.f-analogs_slow[0])*4.0f);
+  filter2.frequency((1024.f-analogs_slow[0])*4.0f);
   AudioInterrupts();
   /*
   if(millis() - last_time >= 5000) {
