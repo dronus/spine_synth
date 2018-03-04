6fb1d87be6f65866aefd6a7f27b350e2ffec6f66
dronus <paul.geisler@web.de>
2018-03-04 21:03:05 +0100

    Replaced linear envelope generators by exponential ones (like R-C integrating circuits)

---
+++ AudioEffectIntegrator.h
@@ -0,0 +1,60 @@
+
+class AudioEffectIntegrator : public AudioStream
+{
+public:
+	AudioEffectIntegrator() : AudioStream(1, inputQueueArray) {
+		attack(0.01f);
+		decay(1.f);
+	}
+	using AudioStream::release;
+  void noteOn(float velocity)
+  {
+  	__disable_irq();
+    energy_in=velocity-energy;
+    __enable_irq();
+  }
+  void pulse(float velocity)
+  {
+  	__disable_irq();
+    energy_in+=velocity;
+    if(energy+energy_in>1.f) energy_in=1.f-energy;
+    __enable_irq();
+  }
+	void attack(float milliseconds) {
+		attack_valve = pow(0.1,1.f/(AUDIO_SAMPLE_RATE_EXACT*0.001f*milliseconds));
+	}
+	void decay(float milliseconds) {
+		decay_valve = pow(0.1,1.f/(AUDIO_SAMPLE_RATE_EXACT*0.001f*milliseconds));
+	}
+	virtual void update(void)	{
+	  int16_t *p, *end;
+
+	  audio_block_t* block = receiveWritable();
+	  if (!block) return;
+
+	  p = (int16_t*)(block->data);
+	  end = p + AUDIO_BLOCK_SAMPLES;
+
+    Serial.print(attack_valve); Serial.print(" ");
+    Serial.print(decay_valve); Serial.print(" ");
+    Serial.print(attack_valve); Serial.print(" ");
+    Serial.print(energy_in); Serial.print(" ");
+    Serial.println(energy);Serial.println();
+
+    double attack_flow=1.-attack_valve;
+	  while (p < end) {
+      double de=energy_in*attack_flow;
+      energy    = energy*decay_valve + de;
+      energy_in = energy_in          - de;
+      (*p++)*=energy;
+	  }
+	  transmit(block);
+	  release(block);
+  }
+private:
+	audio_block_t *inputQueueArray[1];
+  double energy_in=0.f,energy=0.f;
+  double attack_valve, decay_valve;
+};
+
+
--- spine_synth.ino
+++ spine_synth.ino
@@ -2,6 +2,7 @@
 #include <math.h> 
 #include <MIDI.h>
 #include <midi_UsbTransport.h>
+#include "AudioEffectIntegrator.h"
 
 // Spine Synth
 //
@@ -25,13 +26,13 @@
 
 
 AudioSynthWaveformDc     dc1;
-AudioEffectEnvelope      env1;
+AudioEffectIntegrator    env1;
 AudioSynthWaveform       osc1;
 AudioSynthWaveform       osc2;
 AudioMixer4              mixer1;
 AudioFilterStateVariable filter1;
 AudioFilterStateVariable filter2;
-AudioEffectEnvelope      env2;
+AudioEffectIntegrator    env2;
 AudioFilterStateVariable filter3;
 AudioOutputAnalog        dac1;
 AudioOutputUSB           usb2;
@@ -80,7 +81,6 @@ void setup(void)
   env1.attack(3.0f); // attack as by TB-303
   env2.attack(3.0f);
   // env2.decay(16.0f); // "TB-303 has 8ms full on and 8ms linear decay" who said this?
-  env2.decay(3000.f); // "TB-303 VEG has 3s decay" (Devilfish docs)
   filter3.frequency(5000.);
   Serial.println("spine_synth running.");
 }
@@ -228,20 +228,23 @@ void loop()
     if(accent_integral>1.0f) accent_integral=1.0f;
 
     // for accented notes, TB-303 decay would be 200ms. We tie that to our cycle, so adjust to 200ms for 120bpm.
-    float decay=accents[step] ? base_cycle_length * 1.6f : log_pot(analogs[5])*4.f*base_cycle_length;
+    float decay=accents[step] ? base_cycle_length * 1.6f : log_pot(analogs[5])*32.f*base_cycle_length;
 
     AudioNoInterrupts();
-    env1.attack(accents[step] ? base_cycle_length*analogs[3]/1024.f : 10.f);  // accent slew tied to 'resonance' pot like TB-303 does.
+    env1.attack(accents[step] ? base_cycle_length*analogs[3]/1024.f : 3.f);  // accent slew tied to 'resonance' pot like TB-303 does.
     env1.decay(decay);
-    env2.sustain(accent_integral*0.5f+0.5f); // TODO TB-303 would add the slewed, slow env1 to VCA for accent notes. Here we just raise sustain for now.
+
+    //env2.sustain(accent_integral*0.5f+0.5f); // TODO TB-303 would add the slewed, slow env1 to VCA for accent notes. Here we just raise sustain for now.
+    env2.decay(300.f); // "TB-303 VEG has 3s decay" (Devilfish docs)
+
     if(frequencies[step]>0 && (!last_slide || last_frequency==0) ) {
-      env1.noteOn();
-      env2.noteOn();
+      env1.noteOn(accent_integral*0.5f+0.5f);
+      env2.noteOn(accent_integral*0.5f+0.5f);
     }
-    if(frequencies[step]==0) {
+/*    if(frequencies[step]==0) {
       env1.noteOff(); 
       env2.noteOff();
-    }
+    }*/
     AudioInterrupts();
 
     digitalWrite(13,step%4==0);
